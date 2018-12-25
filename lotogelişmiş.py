02224eedf7410f12dc18f97823964f491c201990
kercinul <46047021+kercinul@users.noreply.github.com>
2018-12-25 21:01:18 +0300

    Add files via upload
    geliştirildi

---
+++ lotogelişmiş.py
@@ -0,0 +1,122 @@
+import sys
+import os
+import time
+import random
+def find (a):
+	if q1 != q and w1 != w and e1 != e and r1 != r and t1 != t and y1 != y:
+		time.sleep(3)
+		print("""
+				HİÇBİR ŞEY KAZANAMADIN 
+				TEKRAR İÇİN 0 
+				ÇIKIŞ İÇİN 1  """)
+		else:
+			print("")
+	
+	if q1 != q and w1 != w and e1 != e and r1 != r and t1 != t and y1 == y:
+		
+		time.sleep(3)
+		print(""" 
+		         SON SAYI """y"""'DEN DOLAYI 25 LİRA KAZANDIN
+				 TEKRAR İÇİN 0
+				 ÇIKIŞ İÇİN 1 """)
+		else:
+			print("")
+		
+		
+		
+		
+		
+	if q1 != q and w1 != w and e1 != e and r1 != r and t1 == t and y1 == y:
+		time.sleep(3)
+		print(""" 
+		         SON 2 SAYI """t , y """'DEN DOLAYI 50 LİRA KAZANDIN
+				 TEKRAR İÇİN 0
+				 ÇIKIŞ İÇİN 1 """)
+		else:
+			print("")
+		
+		
+		
+	if q1 != q and w1 != w and e1 != e and r1 == r and t1 == t and y1 == y:
+		time.sleep(3)
+		print(""" 
+		         SON 3 SAYI """r , t , y """'DEN DOLAYI 100 LİRA KAZANDIN
+				 TEKRAR İÇİN 0
+				 ÇIKIŞ İÇİN 1 """)
+		else:
+			print("")
+		
+		
+	if q1 != q and w1 != w and e1 == e and r1 == r and t1 == t and y1 == y:
+		time.sleep(3)
+		print(""" 
+		         SON 4 SAYI """ e , r , t , y """'DEN DOLAYI 5.000 LİRA KAZANDIN
+				 TEKRAR İÇİN 0
+				 ÇIKIŞ İÇİN 1 """)
+		else:
+			print("")
+			
+			
+	if q1 != q and w1 == w and e1 == e and r1 == r and t1 == t and y1 == y:
+		time.sleep(3)
+		print(""" 
+		         SON 5 SAYI """w , e , r , t , y """'DEN DOLAYI 10.000 LİRA KAZANDIN
+				 TEKRAR İÇİN 0
+				 ÇIKIŞ İÇİN 1 """)
+		else:
+			print("")
+	if q1 == q and w1 == w and e1 == e and r1 == r and t1 == t and y1 == y:
+		time.sleep(10)
+		print(""" 
+		         TEBRİKLER BÜYÜK İKRAMİYE SANA VURDU BALLI PİÇ SENİ BÜYÜK ÖDÜL
+				 999.999.999.999.999.999 $ + € + TL ORTAYA KARIŞIK DÖVİZ TABAĞI VE
+				 18 ADET BÖBREK ÖDÜLÜN
+				 TEKRAR İÇİN 0
+				 ÇIKIŞ İÇİN 1 """)
+		else:
+			print("")
+	print("kazan numaralar"q,w,e,r,t,y)
+	global c 
+	c = int(input(""))
+	return a 
+cn = []	
+numbers = []
+q = random.randrange(1,49)
+w = random.randrange(1,49)
+e = random.randrange(1,49)
+r = random.randrange(1,49)
+t = random.randrange(1,49)
+y = random.randrange(1,49)
+numbers.append(q)
+numbers.append(w)
+numbers.append(e)
+numbers.append(r)
+numbers.append(t)
+numbers.append(y)
+file = 1
+
+while file == 1:
+	q1 = int(input(""))
+	w1 = int(input(""))
+	e1 = int(input(""))
+	r1 = int(input(""))
+	t1 = int(input(""))
+	y1 = int(input(""))
+	cn.append(q1)
+	cn.append(w1)
+	cn.append(e1)
+	cn.append(r1)
+	cn.append(t1)
+	cn.append(y1)
+	print find(b)
+	if c == 0:
+		break
+	if c == 1:
+		print("yeni sayılar")
+	os.system('cls')
+
+	
+
+	
+	
+	
